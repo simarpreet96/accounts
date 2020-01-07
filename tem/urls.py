9876096bdf02bdd6c9668e97a142d3a2c4f8eb54
simarpreet96 <57700976+simarpreet96@users.noreply.github.com>
2020-01-07 22:59:31 +0530

    Add files via upload

---
+++ tem/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import SignUpView,LogInView
+
+urlpatterns = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LogInView.as_view(), name='login'),
+]
---
+++ tem/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm,CustomUserChangeForm
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+class LogInView(CreateView):
+	form_class=CustomUserChangeForm
+	success_url=reverse_lazy('login')
+	template_name='registration/login.html'
